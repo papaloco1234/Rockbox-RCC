6331831069b72ce2e0ed86f797005f1f2894c39b
papaloco1234 <rock1104.tw@yahoo.com.tw>
2014-05-27 16:40:16 +0200

    remove isBtmAlive, since it cause trouble when activity onDestory()

--- android/src/org/rockbox/RockboxFramebuffer.java
+++ android/src/org/rockbox/RockboxFramebuffer.java
@@ -136,17 +136,6 @@ public class RockboxFramebuffer extends SurfaceView
         }
     }
 
-    private boolean isBtmAlive()
-    {
-        if (btm.isRecycled() == true || btm == null) 
-        {  
-           if (srcWidth !=0 && srcHeight !=0)
-                btm = Bitmap.createBitmap(srcWidth, srcHeight, Bitmap.Config.RGB_565);
-           else
-                return false; 
-        } 
-        return true;           
-    }
 
     private void update(ByteBuffer framebuffer)
     {
@@ -154,8 +143,6 @@ public class RockboxFramebuffer extends SurfaceView
         Canvas c;
         Rect dirty = new Rect();
 
-        if (isBtmAlive() == false)
-            return;  
 
         if (downscaled && fastScale == true) //downscale
         {
@@ -198,8 +185,6 @@ public class RockboxFramebuffer extends SurfaceView
             scaledDirty.set((int)(dirty.left * scaleWidthFactor), (int)(dirty.top * scaleHeightFactor),
                         (int)(dirty.right * scaleWidthFactor), (int)(dirty.bottom * scaleHeightFactor));    
 
-        if (isBtmAlive() == false)
-            return; 
   
         Canvas c = holder.lockCanvas(scaledDirty);
         if (c == null) 
