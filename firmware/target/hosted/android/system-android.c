Rockbox/rockbox

RaaA: Android work around the Kitkat ART crashes
fixed the RockboxPCM stop() crash.(RockboxPCM JNI)
fixed notification crash.(Service JNI)
fixed save cfg crash.(keyboard JNI)

change the Android arm build script to fix the scrolling text crash

Change-Id: I21d00aa669295a615bc203555d57ca9cebb20da7

firmware/target/hosted/android/system-android.c
@@ -111,8 +111,9 @@ Java_org_rockbox_RockboxService_main(JNIEnv *env, jobject this)
     {
         env_ptr = env;
 
-        RockboxService_instance = this;
-        RockboxService_class = (*env)->GetObjectClass(env, this);
+        RockboxService_instance = (*env)->NewGlobalRef(env_ptr,this);
+        jclass local_RockboxService_class = (*env)->GetObjectClass(env, this);
+        RockboxService_class =(*env)->NewGlobalRef(env_ptr,local_RockboxService_class);
 
         main();
     }

papaloco1234/Rockbox-RCC

firmware/target/hosted/android/system-android.c
@@ -111,8 +111,9 @@ Java_org_rockbox_RockboxService_main(JNIEnv *env, jobject this)
     {
         env_ptr = env;
 
-        RockboxService_instance = this;
-        RockboxService_class = (*env)->GetObjectClass(env, this);
+        RockboxService_instance = (*env)->NewGlobalRef(env_ptr,this);
+        jclass cls = (*env)->GetObjectClass(env, this);
+        RockboxService_class =(*env)->NewGlobalRef(env_ptr,cls);
 
         main();
     }